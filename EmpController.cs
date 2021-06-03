12b972e692b4b020475b91e291bcd3d01003bbca
shiva <50239824+shivaconceptsolution@users.noreply.github.com>
2021-06-03 10:04:03 +0530

    Add files via upload

---
+++ EmpController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataFirst.Models;
+
+namespace DataFirst.Controllers
+{
+    public class EmpController : Controller
+    {
+        private Database1Entities db = new Database1Entities();
+
+        //
+        // GET: /Emp/
+
+        public ActionResult Index()
+        {
+            return View(db.Employees.ToList());
+        }
+
+        //
+        // GET: /Emp/Details/5
+
+        public ActionResult Details(string id = null)
+        {
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        //
+        // GET: /Emp/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Emp/Create
+
+        [HttpPost]
+        public ActionResult Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Employees.Add(employee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(employee);
+        }
+
+        //
+        // GET: /Emp/Edit/5
+
+        public ActionResult Edit(string id = null)
+        {
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        //
+        // POST: /Emp/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(employee).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+
+        //
+        // GET: /Emp/Delete/5
+
+        public ActionResult Delete(string id = null)
+        {
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        //
+        // POST: /Emp/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Employee employee = db.Employees.Find(id);
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ LoginController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataFirst.Models;
+namespace DataFirst.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Login/
+        Database1Entities db = new Database1Entities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Login obj)
+        {
+            var s = (from c in db.Logins where c.UserId.Equals(obj.UserId) && c.Password.Equals(obj.Password) select c).FirstOrDefault();
+            if (s != null)
+            {
+                return RedirectToAction("Index", "Emp");
+            }
+            ViewBag.data = "Invalid userid and password";
+            return View();
+        }
+
+    }
+}
